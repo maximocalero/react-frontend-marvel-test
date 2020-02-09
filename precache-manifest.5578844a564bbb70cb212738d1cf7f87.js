03c5739dc90cecaa57c7eb1b82d9b11bdbd1da84
Maximo Calero Sanchez (GitHub) <maximo.calero@outlook.com>
2020-02-09 08:32:36 +0100

    Updates

---
+++ precache-manifest.5578844a564bbb70cb212738d1cf7f87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3490de54e1a54f15d09816dbada2562a",
+    "url": "/react-frontend-marvel-test/index.html"
+  },
+  {
+    "revision": "962eae4085d43342151a",
+    "url": "/react-frontend-marvel-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6463f07f2e97213082a5",
+    "url": "/react-frontend-marvel-test/static/js/2.c4213787.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-frontend-marvel-test/static/js/2.c4213787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962eae4085d43342151a",
+    "url": "/react-frontend-marvel-test/static/js/main.f6b19eb5.chunk.js"
+  },
+  {
+    "revision": "fd5bcf223d7156c2d7e6",
+    "url": "/react-frontend-marvel-test/static/js/runtime-main.bbc90140.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-frontend-marvel-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
